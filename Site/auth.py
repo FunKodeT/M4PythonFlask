d2af1259a7a0f3ee7b1b3383097ef12c6a786fdd
FunKodeT <cooteybug@gmail.com>
2024-08-13 17:53:02 -0600

    -1; Commited for workstation change;

--- Site/auth.py
+++ Site/auth.py
@@ -121,7 +121,7 @@
 #:::::::::::::::::::::::::::::::::::::::::::::::\
 ## IMPORTS
 #-------------------------------------<
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 #_______________________________________________/
 #:::::::::::::::::::::::::::::::::::::::::::::::\
 ## BLUEPRINT
@@ -134,7 +134,7 @@ auth = Blueprint('auth', __name__)
 #---------------------------<
 #region <LOGIN ROUTE>
 #---------------<
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 #---------------<
 # LOGIN: FUNCTION
 #----------<
@@ -156,16 +156,213 @@ def logout():
 #---------------------------<
 #region <SIGN-UP ROUTE>
 #---------------<
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 #---------------<
 # SIGN-UP: FUNCTION
 #----------<
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password = request.form.get('password')
+        if len(email) < 4:
+            flash('Email is too short, must be longer than 4 characters', category='warning')
+            pass
+        elif len(firstName) < 2:
+            pass
+            flash('First Name is too short, must be longer than 2 characters', category='warning')
+        elif password1 != password:
+            pass
+            flash('Passwords do not match, try again', category='warning')
+        elif len(password1) < 7:
+            pass
+            flash('Password is too short, must be longer than 7 characters', category='warning')
+        else:
+            flash('Account was successfully created, welcome!', category='success')
+
     return render_template('sign_up.html')
 
 #endregion
 #:::::::::::::::::::::::::::::::::::::::::::::::<
 #=========================================================<
+#endregion
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#region <PROGRAM0_V0>
+# #=========================================================<
+# #_______________________________________________/
+# #:::::::::::::::::::::::::::::::::::::::::::::::\
+# ## IMPORTS
+# #-------------------------------------<
+# from flask import Blueprint, render_template, request
+# #_______________________________________________/
+# #:::::::::::::::::::::::::::::::::::::::::::::::\
+# ## BLUEPRINT
+# #:::::::::::::::::::::::::::::::::::::<
+# # BLUEPRINT: AUTH
+# #---------------------------<
+# auth = Blueprint('auth', __name__)
+# #-------------------------------------<
+# # BLUEPRINT: ROUTES
+# #---------------------------<
+# #region <LOGIN ROUTE>
+# #---------------<
+# @auth.route('/login', methods=['GET', 'POST'])
+# #---------------<
+# # LOGIN: FUNCTION
+# #----------<
+# def login():
+#     return render_template('login.html', boolean=True)
+
+# #endregion
+# #---------------------------<
+# #region <LOGOUT ROUTE>
+# #---------------<
+# @auth.route('/logout')
+# #---------------<
+# # LOGOUT: FUNCTION
+# #----------<
+# def logout():
+#     return '<p>Logout</p>'
+
+# #endregion
+# #---------------------------<
+# #region <SIGN-UP ROUTE>
+# #---------------<
+# @auth.route('/sign-up', methods=['GET', 'POST'])
+# #---------------<
+# # SIGN-UP: FUNCTION
+# #----------<
+# def sign_up():
+#     if request.method == 'POST':
+#         email = request.form.get('email')
+#         firstName = request.form.get('firstName')
+#         password1 = request.form.get('password1')
+#         password = request.form.get('password')
+#         if len(email) < 4:
+#             pass
+#         elif len(firstName) < 2:
+#             pass
+#         elif password1 != password:
+#             pass
+#         elif len(password1) < 7:
+#             pass
+#         else:
+#             print('temp')
+
+#     return render_template('sign_up.html')
+
+# #endregion
+# #:::::::::::::::::::::::::::::::::::::::::::::::<
+# #=========================================================<
+#endregion
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#region <PROGRAM0_V0>
+# #=========================================================<
+# #_______________________________________________/
+# #:::::::::::::::::::::::::::::::::::::::::::::::\
+# ## IMPORTS
+# #-------------------------------------<
+# from flask import Blueprint, render_template, request
+# #_______________________________________________/
+# #:::::::::::::::::::::::::::::::::::::::::::::::\
+# ## BLUEPRINT
+# #:::::::::::::::::::::::::::::::::::::<
+# # BLUEPRINT: AUTH
+# #---------------------------<
+# auth = Blueprint('auth', __name__)
+# #-------------------------------------<
+# # BLUEPRINT: ROUTES
+# #---------------------------<
+# #region <LOGIN ROUTE>
+# #---------------<
+# @auth.route('/login', methods=['GET', 'POST'])
+# #---------------<
+# # LOGIN: FUNCTION
+# #----------<
+# def login():
+#     data = request.form
+#     print(data)
+#     return render_template('login.html', boolean=True)
+
+# #endregion
+# #---------------------------<
+# #region <LOGOUT ROUTE>
+# #---------------<
+# @auth.route('/logout')
+# #---------------<
+# # LOGOUT: FUNCTION
+# #----------<
+# def logout():
+#     return '<p>Logout</p>'
+
+# #endregion
+# #---------------------------<
+# #region <SIGN-UP ROUTE>
+# #---------------<
+# @auth.route('/sign-up', methods=['GET', 'POST'])
+# #---------------<
+# # SIGN-UP: FUNCTION
+# #----------<
+# def sign_up():
+#     return render_template('sign_up.html')
+
+# #endregion
+# #:::::::::::::::::::::::::::::::::::::::::::::::<
+# #=========================================================<
+#endregion
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+#region <PROGRAM0_V0>
+# #=========================================================<
+# #_______________________________________________/
+# #:::::::::::::::::::::::::::::::::::::::::::::::\
+# ## IMPORTS
+# #-------------------------------------<
+# from flask import Blueprint, render_template
+# #_______________________________________________/
+# #:::::::::::::::::::::::::::::::::::::::::::::::\
+# ## BLUEPRINT
+# #:::::::::::::::::::::::::::::::::::::<
+# # BLUEPRINT: AUTH
+# #---------------------------<
+# auth = Blueprint('auth', __name__)
+# #-------------------------------------<
+# # BLUEPRINT: ROUTES
+# #---------------------------<
+# #region <LOGIN ROUTE>
+# #---------------<
+# @auth.route('/login')
+# #---------------<
+# # LOGIN: FUNCTION
+# #----------<
+# def login():
+#     return render_template('login.html', boolean=True)
+
+# #endregion
+# #---------------------------<
+# #region <LOGOUT ROUTE>
+# #---------------<
+# @auth.route('/logout')
+# #---------------<
+# # LOGOUT: FUNCTION
+# #----------<
+# def logout():
+#     return '<p>Logout</p>'
+
+# #endregion
+# #---------------------------<
+# #region <SIGN-UP ROUTE>
+# #---------------<
+# @auth.route('/sign-up')
+# #---------------<
+# # SIGN-UP: FUNCTION
+# #----------<
+# def sign_up():
+#     return render_template('sign_up.html')
+
+# #endregion
+# #:::::::::::::::::::::::::::::::::::::::::::::::<
+# #=========================================================<
 #endregion
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 #region <PROGRAM0_V0>
